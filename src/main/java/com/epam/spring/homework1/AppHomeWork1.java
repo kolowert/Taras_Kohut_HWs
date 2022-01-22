1bba967632d8d163cd5deeadf1358de74773200b
taras kohut <tiarkey@yahoo.com>
2022-01-22 16:03:41 +0200

    playing bean configuration

--- src/main/java/com/epam/spring/homework1/AppHomeWork1.java
+++
@@ -1,9 +0,0 @@
-package com.epam.spring.homework1;
-
-public class AppHomeWork1 {
-
-	public static void main(String[] args) {
-		System.out.println("Home work 1 starts here");
-	}
-
-}
---
+++ src/main/java/com/epam/spring/homework1/config/OtherConfig.java
@@ -0,0 +1,10 @@
+package com.epam.spring.homework1.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.epam.homework1.other")
+public class OtherConfig {
+
+}
