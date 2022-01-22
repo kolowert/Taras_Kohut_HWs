f17b56f48a4c0cdbb057dc5c152b3728a717fa75
taras kohut <tiarkey@yahoo.com>
2022-01-22 15:10:26 +0200

    init

---
+++ src/main/java/com/epam/spring/homeworks/App.java
@@ -0,0 +1,11 @@
+package com.epam.spring.homeworks;
+
+/**
+ * Hello-hello!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Here we are");
+	}
+}
---
+++ src/test/java/com/epam/spring/homeworks/AppTest.java
@@ -0,0 +1,5 @@
+package com.epam.spring.homeworks;
+
+public class AppTest {
+
+}
